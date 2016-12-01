40050f97f548bdd54cd3047d12c9775fea93bb6d
ccmcount <ccmcount@gmail.com>
2016-12-01 17:11:50 +0900

    commit

---
+++ Assets/testCode1.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testCode1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        int i = 0;
+        Debug.Log(i);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
